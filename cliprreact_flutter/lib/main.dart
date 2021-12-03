346324d52cb3e759f74f5b1c49703bf1189dd494
hammad-qayyum <hammadqayyum1047@gmail.com>
2021-12-03 23:51:46 +0500

    1st commit

---
+++ cliprreact_flutter/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ClipRRect Flutter'),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+          child: ClipRRect(
+            borderRadius:
+            BorderRadius.circular(20),
+              child: Image.network('https://picsum.photos/250?image=1'),
+            ),
+          )
+      );
+  }
+}
+
+class MyClip extends CustomClipper<Rect> {
+  Rect getClip(Size size) {
+    return Rect.fromLTWH(0, 0, 100, 100);
+  }
+ 
+  bool shouldReclip(oldClipper) {
+    return false;
+  }
+}
